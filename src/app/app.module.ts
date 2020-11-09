cbc248279f1d0840ce2672b60d45fb3a6b526bcf
FunesM <fernando99funes@gmail.com>
2020-11-08 23:49:19 -0600

    version final editor

---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {ReactiveFormsModule} from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//firebase
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
+//Service
+import { AuthService } from "./services/auth.service";
+import { AuthGuard } from "./guard/auth.guard";
+
+//editor
+import { EditorComponent } from './components/editor/editor.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { CKEditorModule } from 'ng2-ckeditor';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    EditorComponent,
+    AcercaDeComponent
+    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CKEditorModule
+  ],
+  providers: [AuthGuard], //Agregamos a los providers el guard
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
